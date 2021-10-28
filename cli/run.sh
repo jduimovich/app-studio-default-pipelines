44c53f7ecec38ed37881d65783ee5d8ca6fadefe
jduimovich <jduimovich@gmail.com>
2021-10-28 15:04:57 -0400

    debug

--- cli/run.sh
+++ cli/run.sh
@@ -2,5 +2,6 @@ export NS=$(oc config view --minify -o "jsonpath={..namespace}")
 export IMG=image-registry.openshift-image-registry.svc:5000/$NS/a-simple-app
 echo "Namespace: " $NS
 echo "Image: " $IMG
-
+yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml
 yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml | oc create -f -
+
