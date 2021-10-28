457cf7b30b184e88a7c0615a3a5d228a16fed95b
John Duimovich <$MY_GITHUB_EMAIL>
2021-10-28 12:10:54 -0400

    echo $IMG

--- cli/run.sh
+++ cli/run.sh
@@ -1,5 +1,6 @@
 export NS=$(oc config view --minify -o "jsonpath={..namespace}")
 export IMG=image-registry.openshift-image-registry.svc:5000/$NS/a-simple-app
 echo $NS
+echo $IMG
 
-yq -M e ".spec.params[1].value = \"$IMG\"" runners\run-basic-clone-push.yaml | oc create -f -
+yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml | oc create -f -
