cc2a3b2e4831e0bad830fbcfdf122ee5527bdc59
John Duimovich <$MY_GITHUB_EMAIL>
2021-10-28 12:11:30 -0400

    print yq

--- cli/run.sh
+++ cli/run.sh
@@ -3,4 +3,6 @@ export IMG=image-registry.openshift-image-registry.svc:5000/$NS/a-simple-app
 echo $NS
 echo $IMG
 
+yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml
+
 yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml | oc create -f -
