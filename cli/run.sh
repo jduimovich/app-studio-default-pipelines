d09d84ff05d9e1904b545298ba15ee980a193244
John Duimovich <$MY_GITHUB_EMAIL>
2021-10-28 15:07:06 -0400

    darn quotes on WSL

--- cli/run.sh
+++ cli/run.sh
@@ -2,6 +2,10 @@ export NS=$(oc config view --minify -o "jsonpath={..namespace}")
 export IMG=image-registry.openshift-image-registry.svc:5000/$NS/a-simple-app
 echo "Namespace: " $NS
 echo "Image: " $IMG
+
+export REP='"'$IMG'"'
+echo $REP
+
 yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml
 yq -M e ".spec.params[1].value = \"$IMG\"" runners/run-basic-clone-push.yaml | oc create -f -
 
