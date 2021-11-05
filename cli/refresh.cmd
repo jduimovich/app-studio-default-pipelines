c4448e0f83bef76977df4a5169d514fa2443b7d7
John Duimovich <$MY_GITHUB_EMAIL>
2021-11-05 12:19:26 -0400

    reset cleanup

--- cli/refresh.cmd
+++ cli/refresh.cmd
@@ -7,3 +7,6 @@ oc project as-build-protected
 tkn pipelineruns delete --all 
 oc delete -f %CD%\pipelines
 oc apply -f %CD%\pipelines 
+oc delete -f %CD%\policy\repo-white-list 
+oc apply -f policy\repo-white-list\template.yaml
+oc apply -f policy\repo-white-list\constraint.yaml
